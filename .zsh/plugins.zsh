1514ee62c12285d8c9b53ff10bda3d2458327acd
asann3 <39186984+asann3@users.noreply.github.com>
2022-07-22 22:44:44 +0900

    Add Powerlevel10k

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,3 +1,5 @@
+DOTFILES_PATH=$HOME/dotfiles
+
 # Zinit
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
@@ -11,3 +13,16 @@ fi
 source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
+
+
+# Powerlevel10k
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f $DOTFILES_PATH/.zsh/.p10k.zsh ]] || source $DOTFILES_PATH/.zsh/.p10k.zsh
